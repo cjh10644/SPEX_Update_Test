dfb79f1dcd72381f54973d4a142fbd2bc4094f31
Jinhao Chen <cjh10644@hotmail.com>
2022-07-06 22:30:12 +0000

    test result for optimized code

---
+++ Source/spex_update_insert_new_entry.c
@@ -0,0 +1,61 @@
+//------------------------------------------------------------------------------
+// SPEX_Update/spex_update_insert_new_entry.c: insert an entry vi who has no
+// pending scale to a scaled vector v, all v->x[i] will be scaled and S will be
+// 1 after vi is inserted.
+//------------------------------------------------------------------------------
+
+// SPEX_Update: (c) 2020-2021, Jinhao Chen, Timothy A. Davis, Erick
+// Moreno-Centeno, Texas A&M University.  All Rights Reserved.  See
+// SPEX_Update/License for the license.
+
+//------------------------------------------------------------------------------
+
+// Purpose: This function is call to insert vi as i-th entry of vector v, where
+// v could be column of L or row of U in frame j. S is the pending scales for
+// vector v. Since vi has no pending scale factor and v has pending scale
+// factor, we will apply S to all v->x[i] before insert vi into v. S is reset to
+// 1 when finished.
+//
+// NOTE: the value of vi is inserted by swapping with corresponding mpz_t value
+// in v, which should be considered as undefined after this function.
+
+#include "spex_update_internal.h"
+
+SPEX_info spex_update_insert_new_entry
+(
+    mpz_t vi,          // the entry to be inserted as i-th entry of v
+    SPEX_vector *v,   // the vector that would add new entry
+    mpq_t S,          // pending scale for v
+    const int64_t i,   // the index of vi when inserted to v
+    const SPEX_options *option
+)
+{
+    SPEX_info info;
+    int r;
+    int64_t p;
+
+    SPEX_CHECK(SPEX_mpq_cmp_ui(&r, S, 1, 1));
+    if (r != 0) //S != 1
+    {
+        for (p = 0; p < v->nz; p++)
+        {
+            // Since entries in v will be integer after scale, we can
+            // perform division first to make it small, and this
+            // division will preserve integer propety
+            SPEX_CHECK(SPEX_mpz_divexact(v->x[p], v->x[p], SPEX_MPQ_DEN(S)));
+            SPEX_CHECK(SPEX_mpz_mul(v->x[p], v->x[p], SPEX_MPQ_NUM(S)));
+        }
+        SPEX_CHECK(SPEX_mpq_set_ui(S, 1, 1));
+    }
+    // append vi to v
+    if (v->nz == v->nzmax)
+    {
+        // reallocate the nonzero pattern if needed
+        SPEX_CHECK(SPEX_vector_realloc(v, v->nzmax+1, option));
+    }
+    v->i[v->nz] = i;
+    SPEX_CHECK(SPEX_mpz_swap(v->x[v->nz], vi));
+    v->nz ++;
+
+    return SPEX_OK;
+}
---
+++ Source/unused/spex_update_verify.c
@@ -0,0 +1,127 @@
+//------------------------------------------------------------------------------
+// SPEX_Update/spex_update_verify.c: verify if A=LD^(-1)U
+//------------------------------------------------------------------------------
+
+// SPEX_Update: (c) 2020-2021, Jinhao Chen, Timothy A. Davis, Erick
+// Moreno-Centeno, Texas A&M University.  All Rights Reserved.  See
+// SPEX_Update/License for the license.
+
+//------------------------------------------------------------------------------
+
+// Purpose: This function is to verify if A=L(P,:)D^(-1)U(:,Q)
+
+#define SPEX_FREE_ALL                 \
+    SPEX_matrix_free(&b, option);     \
+    SPEX_matrix_free(&x, option);     \
+    SPEX_matrix_free(&b2, option);    \
+    SPEX_MPQ_CLEAR(temp);
+
+#include "spex_update_internal.h"
+
+SPEX_info spex_update_verify
+(
+    SPEX_factorization *F,// LU factorization of A
+    const SPEX_matrix *A,     // Input matrix
+    int64_t *h,            // history vector
+    const SPEX_options *option// command options
+)
+{
+    SPEX_info info;
+    int64_t tmp, i, n = F->L->n;
+    int r;
+    mpq_t temp; SPEX_MPQ_SET_NULL(temp);
+    SPEX_matrix *b = NULL; // the dense right-hand-side matrix to be generated
+    SPEX_matrix *x = NULL; // the dense solution matrix to be generated
+    SPEX_matrix *b2 = NULL; // the dense matrix to store the result of A*x
+
+    SPEX_CHECK(SPEX_mpq_init(temp));
+    SPEX_CHECK(SPEX_matrix_allocate(&b , SPEX_DENSE, SPEX_MPZ, n, 1, n, false,
+        true, option));
+    SPEX_CHECK(SPEX_matrix_allocate(&b2, SPEX_DENSE, SPEX_MPZ, n, 1, n, false,
+        true, option));
+
+    // -------------------------------------------------------------------------
+    // generate random right-hand-size vector
+    // -------------------------------------------------------------------------
+    // initialize random number generator
+    int seed = 10;
+    srand(seed);
+    for (i = 0; i < n; i++)
+    {
+        tmp = i+1;//rand(); //TODO
+        SPEX_CHECK(SPEX_mpz_set_si(b->x.mpz[i], tmp));
+    }
+
+    // -------------------------------------------------------------------------
+    // solve LD^(-1)Ux = b for x
+    // -------------------------------------------------------------------------
+    SPEX_CHECK(SPEX_Update_solve(&x, F, b, option));
+
+    // -------------------------------------------------------------------------
+    // compute b2 = A*x
+    // -------------------------------------------------------------------------
+    for (i = 0; i < n; i++)
+    {
+        SPEX_CHECK(SPEX_mpz_sgn(&r, x->x.mpz[i]));
+        if (r == 0) { continue;}
+
+        for (int64_t p = 0; p < A->v[i]->nz; p++)
+        {
+            int64_t j = A->v[i]->i[p];
+            SPEX_CHECK(SPEX_mpq_set_z(temp, A->v[i]->x[p]));
+            // b2[j] += x[i]*A(j,i)
+            SPEX_CHECK(SPEX_mpq_mul(temp, temp, x->x.mpq[i]));
+            SPEX_CHECK(SPEX_mpq_add(b2->x.mpq[j], b2->x.mpq[j], temp));
+        }
+    }
+    //--------------------------------------------------------------------------
+    // Apply scales of A and b to b2 before comparing the b2 with scaled b'
+    //--------------------------------------------------------------------------
+    SPEX_CHECK(SPEX_mpq_div(temp, b->scale, A->scale));
+
+    // Apply scaling factor, but ONLY if it is not 1
+    SPEX_CHECK(SPEX_mpq_cmp_ui(&r, temp, 1, 1));
+    if (r != 0)
+    {
+        for (i = 0; i < n; i++)
+        {
+            SPEX_CHECK(SPEX_mpq_mul(b2->x.mpq[i], b2->x.mpq[i], temp));
+        }
+    }
+
+    // -------------------------------------------------------------------------
+    // check if b2 == b
+    // -------------------------------------------------------------------------
+    for (i = 0; i < n; i++)
+    {
+        // temp = b[i] (correct b)
+        SPEX_CHECK(SPEX_mpq_set_z(temp, b->x.mpz[i]));
+
+        // set check false if b!=b2
+        SPEX_CHECK(SPEX_mpq_equal(&r, temp, b2->x.mpq[i]));
+        if (r == 0)
+        {
+            info = SPEX_INCORRECT;
+            break;
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    // Print info
+    //--------------------------------------------------------------------------
+
+    int pr = SPEX_OPTION_PRINT_LEVEL (option) ;
+    if (info == SPEX_OK)
+    {
+        SPEX_PR1 ("Factorization is verified to be correct and exact.\n") ;
+    }
+    else if (info == SPEX_INCORRECT)
+    {
+        // This can never happen.
+        SPEX_PR1 ("ERROR! Factorization is wrong. This is a bug; please "
+                  "contact the authors of SPEX.\n") ;
+    }
+
+    SPEX_FREE_ALL;
+    return info;
+}
